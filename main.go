a0932ed337053608db5cb6adf4c38fc73bf37ed6
hjl747073211 <747073211@qq.com>
2020-02-28 17:35:14 +0800

    新增配置文件

--- main.go
+++ main.go
@@ -3,24 +3,36 @@ package main
 //入口程序
 import (
 	"fmt"
+	conf "logAgent/Conf"
 	"logAgent/kafka"
 	"logAgent/taillog"
 	"time"
+
+	"gopkg.in/ini.v1"
 )
 
 //taillog是从日志中读取内容
 //sarama是把日志内容放入kafka
 
+var cfg = new(conf.AppConf)
+
 func main() {
+	//加载配置文件,映射到cfg
+	err := ini.MapTo(cfg, "./conf/config.ini")
+	if err != nil {
+		fmt.Printf("ini load failed,err:%v", err)
+		return
+	}
+
 	//1.初始化kafka的链接
-	err := kafka.Init([]string{"127.0.0.1:9092"})
+	err = kafka.Init([]string{cfg.KafkaConf.Address})
 	if err != nil {
 		fmt.Printf("init kafka failed,err:%v", err)
 		return
 	}
 	fmt.Println("kafka init success")
 	//打开日志文件准备读取日志
-	err = taillog.Init("./my.log") //传入日志文件的路径
+	err = taillog.Init(cfg.TaillogConf.FileName) //传入日志文件的路径
 	if err != nil {
 		fmt.Printf("init tail failed,err:%v", err)
 		return
@@ -35,7 +47,7 @@ func run() {
 		select {
 		case line := <-taillog.ReadChan():
 			//2.发送到kafka
-			kafka.SendToKafka("web_log", line.Text)
+			kafka.SendToKafka(cfg.KafkaConf.Topic, line.Text)
 		default:
 			time.Sleep(time.Second)
 		}
